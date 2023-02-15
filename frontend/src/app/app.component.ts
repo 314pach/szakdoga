2201df00ff34b3429193e30175ccf606a1ee1a8b
Szűcs Eszter <h052431@stud.u-szeged.hu>
2023-02-16 00:40:55 +0100

    Style menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2, ViewChild} from '@angular/core';
+import {MatDrawer, MatDrawerMode} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  @ViewChild("drawer") drawer!: MatDrawer;
+
+  mode: MatDrawerMode = "side";
+
+  threshold: number = 600;
+
+  getScreenWidth(): number {
+    return window.innerWidth;
+  }
+
+  setMenuMode = (e: Event) => {
+    if (this.getScreenWidth() >= this.threshold){
+      if (this.mode === "over"){
+        this.drawer.open();
+      }
+      this.mode = "side";
+    } else {
+      if (this.mode === "side"){
+        this.drawer.close();
+      }
+      this.mode = "over"
+    }
+  }
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen("window", "resize", this.setMenuMode);
+    this.mode = this.getScreenWidth() >= this.threshold ? "side" : "over";
+  }
 }
