4c3f582bcf622494da52cf6a0b04f614ab009d0d
Szűcs Eszter <h052431@stud.u-szeged.hu>
2023-03-13 13:15:05 +0100

    Create login and register UI

--- frontend/src/app/authentication/login/login.component.ts
+++ frontend/src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  emailControl: FormControl = new FormControl<string>("", [Validators.required, Validators.email]);
+  passwordControl: FormControl = new FormControl<string>("", Validators.required);
+
+  constructor() {
+  }
+
+  isDisabled() {
+    return this.emailControl.invalid || this.passwordControl.invalid;
+  }
 
 }
--- frontend/src/app/authentication/register/register.component.ts
+++ frontend/src/app/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  hide1 = true;
+  hide2 = true;
 
+  confirmed: boolean = false;
+
+  nameControl: FormControl = new FormControl<string>("", Validators.required);
+  emailControl: FormControl = new FormControl<string>("", [Validators.required, Validators.email]);
+  passwordControl: FormControl = new FormControl<string>("", [Validators.required, Validators.minLength(6), Validators.maxLength(15), Validators.pattern("(?=[A-Za-z0-9]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$")]);
+  passwordAgainControl: FormControl = new FormControl<string>("", Validators.required);
+
+  constructor() {
+  }
+
+  passwordsMatch() {
+    if (this.passwordControl.value !== this.passwordAgainControl.value) {
+      this.confirmed = false;
+      this.passwordAgainControl.setErrors({'incorrect': true});
+    } else {
+      this.confirmed = true;
+    }
+  }
+
+  isDisabled() {
+    return this.nameControl.invalid || this.emailControl.invalid || this.passwordControl.invalid || this.passwordAgainControl.invalid || !this.confirmed;
+  }
 }
