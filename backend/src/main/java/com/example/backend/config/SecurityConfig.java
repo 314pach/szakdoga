bfed60657f0f01bd41179a0d613adcaa5fc3cb6c
Szűcs Eszter <h052431@stud.u-szeged.hu>
2023-03-31 11:54:41 +0200

    Configure swagger ui

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/authentication/**")
+                .requestMatchers("/authentication/**", "/v3/api-docs*/**", "/swagger-ui*/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
