6ce10c25a64c5673ff03ca9752969368a030aa38
Szűcs Eszter <h052431@stud.u-szeged.hu>
2023-04-16 19:31:15 +0200

    Add file id=1 as an automatic profile picture

--- backend/src/main/java/com/example/backend/auth/AuthenticationService.java
+++ backend/src/main/java/com/example/backend/auth/AuthenticationService.java
@@ -4,10 +4,12 @@ import com.example.backend.config.JwtService;
 import com.example.backend.model.ApplicationUser;
 import com.example.backend.model.Role;
 import com.example.backend.repository.ApplicationUserRepository;
+import com.example.backend.repository.FileRepository;
 import com.example.backend.service.ApplicationUserService;
 import com.example.backend.token.Token;
 import com.example.backend.token.TokenRepository;
 import com.example.backend.token.TokenType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +26,8 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    @Autowired
+    private FileRepository fileRepository;
 
     public AuthenticationService(ApplicationUserRepository applicationUserRepository, ApplicationUserService applicationUserService, TokenRepository tokenRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.applicationUserRepository = applicationUserRepository;
@@ -40,6 +44,7 @@ public class AuthenticationService {
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(Role.STUDENT);
+        user.setProfilePicture(fileRepository.findById(1L).orElse(null));
         user.setClassRooms(new HashSet<>());
         user.setCommitments(new HashSet<>());
 
